67fd3a7be5f36688e670d109603a06a1b98d120b
jowlly <98984146+jowlly@users.noreply.github.com>
2025-05-28 21:42:21 +0300

    trimtxt + rename person

---
+++ GalleryNestServer/GalleryNestApp/View/TrimTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GalleryNestApp.View
+{
+    class TrimTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            string text = value.ToString()??"";
+            int maxLength = 15; 
+
+            if (text.Length <= maxLength)
+                return text;
+            else
+                return text.Substring(0, maxLength) + "…"; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
